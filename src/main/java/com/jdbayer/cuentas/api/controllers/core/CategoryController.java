267323e0399e06f189e36e8ee0d5fb688b702bcd
Julian Bayer <jdbayer09@hotmail.com>
2024-04-16 15:07:11 -0500

    Creacion de controller

---
+++ src/main/java/com/jdbayer/cuentas/api/controllers/core/CategoryController.java
@@ -0,0 +1,29 @@
+package com.jdbayer.cuentas.api.controllers.core;
+
+import com.jdbayer.cuentas.api.models.responses.base.ErrorResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category/{idUser}")
+@RequiredArgsConstructor
+@Tag(name = "Controlador de categorías", description = "Controlador encargado de administrar las categorías")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "403", description = "Credentials are required to access this resource", content =
+        @Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = @Schema(implementation = ErrorResponse.class)))
+})
+@SecurityRequirement(name = "Bearer Authentication")
+public class CategoryController {
+
+
+}
