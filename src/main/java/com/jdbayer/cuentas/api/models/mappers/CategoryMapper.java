a366fed5926575b73343a8cbdcbe5216a264d677
Julian Bayer <jdbayer09@hotmail.com>
2024-04-16 16:22:59 -0500

    creacion de funciones para convertir un DTO en un response

--- src/main/java/com/jdbayer/cuentas/api/models/mappers/CategoryMapper.java
+++ src/main/java/com/jdbayer/cuentas/api/models/mappers/CategoryMapper.java
@@ -1,9 +1,13 @@
 package com.jdbayer.cuentas.api.models.mappers;
 
+import com.jdbayer.cuentas.api.models.dto.core.CategoryDTO;
+import com.jdbayer.cuentas.api.models.responses.core.BaseCategoryResponse;
+import com.jdbayer.cuentas.api.models.responses.core.CategoryResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CategoryMapper {
-
+    BaseCategoryResponse dtoToBaseResponse(CategoryDTO dto);
+    CategoryResponse dtoToResponse(CategoryDTO dto);
 }
