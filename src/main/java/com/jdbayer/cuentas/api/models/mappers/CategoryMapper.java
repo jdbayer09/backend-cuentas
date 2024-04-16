0ea0fd7d4cbbdfd9ff7403d287bec1ba8c2970ce
Julian Bayer <jdbayer09@hotmail.com>
2024-04-16 14:54:56 -0500

    Creacion de Mapper

---
+++ src/main/java/com/jdbayer/cuentas/api/models/mappers/CategoryMapper.java
@@ -0,0 +1,9 @@
+package com.jdbayer.cuentas.api.models.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CategoryMapper {
+    
+}
