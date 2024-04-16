9ce6effc44b1356e178fb8e3d307f766caa8f2fc
Julian Bayer <jdbayer09@hotmail.com>
2024-04-16 16:23:16 -0500

    configuracion base del servicio

--- src/main/java/com/jdbayer/cuentas/api/services/core/ICategoryService.java
+++ src/main/java/com/jdbayer/cuentas/api/services/core/ICategoryService.java
@@ -1,4 +1,16 @@
 package com.jdbayer.cuentas.api.services.core;
 
+import com.jdbayer.cuentas.api.models.dto.admin.UserDTO;
+import com.jdbayer.cuentas.api.models.dto.core.CategoryDTO;
+import com.jdbayer.cuentas.api.models.requests.core.CategoryRequest;
+
+import java.util.List;
+
 public interface ICategoryService {
+    CategoryDTO createCategory(UserDTO user, CategoryRequest request);
+    CategoryDTO updateCategory(UserDTO user, CategoryRequest request, Long idCategory);
+    void disableCategory(Long idCategory);
+    void enableCategory(Long idCategory);
+    List<CategoryDTO> getAllCategories(UserDTO user);
+    List<CategoryDTO> getActiveCategories(UserDTO user);
 }
--- src/main/java/com/jdbayer/cuentas/api/services/core/impl/CategoryServiceImpl.java
+++ src/main/java/com/jdbayer/cuentas/api/services/core/impl/CategoryServiceImpl.java
@@ -1,10 +1,45 @@
 package com.jdbayer.cuentas.api.services.core.impl;
 
+import com.jdbayer.cuentas.api.models.dto.admin.UserDTO;
+import com.jdbayer.cuentas.api.models.dto.core.CategoryDTO;
+import com.jdbayer.cuentas.api.models.requests.core.CategoryRequest;
 import com.jdbayer.cuentas.api.services.core.ICategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CategoryServiceImpl implements ICategoryService {
+    @Override
+    public CategoryDTO createCategory(UserDTO user, CategoryRequest request) {
+        return null;
+    }
+
+    @Override
+    public CategoryDTO updateCategory(UserDTO user, CategoryRequest request, Long idCategory) {
+        return null;
+    }
+
+    @Override
+    public void disableCategory(Long idCategory) {
+
+    }
+
+    @Override
+    public void enableCategory(Long idCategory) {
+
+    }
+
+    @Override
+    public List<CategoryDTO> getAllCategories(UserDTO user) {
+        return List.of();
+    }
+
+    @Override
+    public List<CategoryDTO> getActiveCategories(UserDTO user) {
+        return List.of();
+    }
+
 }
