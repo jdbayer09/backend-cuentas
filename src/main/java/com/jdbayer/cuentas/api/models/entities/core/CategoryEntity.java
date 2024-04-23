58540b8f852c97a9e2c972ec30dd169663d9d3f8
Julian Bayer <jdbayer09@hotmail.com>
2024-04-23 12:26:33 -0500

    Cambio de abstraccion para el manejo de los usuarios

--- src/main/java/com/jdbayer/cuentas/api/models/entities/core/CategoryEntity.java
+++ src/main/java/com/jdbayer/cuentas/api/models/entities/core/CategoryEntity.java
@@ -1,16 +1,11 @@
 package com.jdbayer.cuentas.api.models.entities.core;
 
-import com.jdbayer.cuentas.api.models.entities.BaseEntity;
-import com.jdbayer.cuentas.api.models.entities.admin.UserEntity;
+import com.jdbayer.cuentas.api.models.entities.BaseUserEntity;
 import com.jdbayer.cuentas.api.models.enums.Color;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
@@ -26,7 +21,7 @@ import java.io.Serial;
 @Getter
 @Setter
 @ToString
-public class CategoryEntity extends BaseEntity {
+public class CategoryEntity extends BaseUserEntity {
 
     @Serial
     private static final long serialVersionUID = -1900890954992882033L;
@@ -43,10 +38,6 @@ public class CategoryEntity extends BaseEntity {
 
     @Column(name = "description", columnDefinition = "TEXT")
     private String description;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_user", nullable = false, foreignKey = @ForeignKey(name = "user_id_category_fk"))
-    private UserEntity user;
 
     @Column(name = "is_active", nullable = false)
     private boolean active;
--- src/main/java/com/jdbayer/cuentas/api/models/entities/core/PaymentMethodEntity.java
+++ src/main/java/com/jdbayer/cuentas/api/models/entities/core/PaymentMethodEntity.java
@@ -1,16 +1,11 @@
 package com.jdbayer.cuentas.api.models.entities.core;
 
-import com.jdbayer.cuentas.api.models.entities.BaseEntity;
-import com.jdbayer.cuentas.api.models.entities.admin.UserEntity;
+import com.jdbayer.cuentas.api.models.entities.BaseUserEntity;
 import com.jdbayer.cuentas.api.models.enums.Color;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
@@ -26,7 +21,7 @@ import java.io.Serial;
 @Getter
 @Setter
 @ToString
-public class PaymentMethodEntity extends BaseEntity {
+public class PaymentMethodEntity extends BaseUserEntity {
 
     @Serial
     private static final long serialVersionUID = 5046404703385700084L;
@@ -43,10 +38,6 @@ public class PaymentMethodEntity extends BaseEntity {
 
     @Column(name = "description", columnDefinition = "TEXT")
     private String description;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_user", nullable = false, foreignKey = @ForeignKey(name = "user_id_payment_methods_fk"))
-    private UserEntity user;
 
     @Column(name = "is_active", nullable = false)
     private boolean active;
