8bd584ebc08b6a758afc29579e1c457123bbb5e6
Julian Bayer <jdbayer09@hotmail.com>
2024-04-23 14:36:53 -0500

    Configuracion de mapper

--- src/main/java/com/jdbayer/cuentas/api/models/mappers/CashReceiptMapper.java
+++ src/main/java/com/jdbayer/cuentas/api/models/mappers/CashReceiptMapper.java
@@ -1,8 +1,14 @@
 package com.jdbayer.cuentas.api.models.mappers;
 
+import com.jdbayer.cuentas.api.models.dto.core.CashReceiptDTO;
+import com.jdbayer.cuentas.api.models.entities.core.CashReceiptEntity;
+import com.jdbayer.cuentas.api.models.responses.core.BaseCashReceiptResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CashReceiptMapper {
+
+    BaseCashReceiptResponse dtoToBaseResponse(CashReceiptDTO dto);
+    CashReceiptDTO entityToDto(CashReceiptEntity entity);
 }
