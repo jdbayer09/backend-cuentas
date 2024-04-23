dac9a1bc5dff7fd420ac5a3d30b50dbd8313b505
Julian Bayer <jdbayer09@hotmail.com>
2024-04-23 14:35:50 -0500

    Configuracion de response base

---
+++ src/main/java/com/jdbayer/cuentas/api/models/responses/core/BaseCashReceiptResponse.java
@@ -0,0 +1,23 @@
+package com.jdbayer.cuentas.api.models.responses.core;
+
+import com.jdbayer.cuentas.api.models.enums.Color;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.math.BigDecimal;
+
+@Schema(description = "Información de respuesta de un ingreso")
+public record BaseCashReceiptResponse(
+        @Schema(description = "ID ingreso")
+        Long id,
+        @Schema(description = "Nombre ingreso")
+        String name,
+        @Schema(description = "Color para identificar el ingreso")
+        Color color,
+        @Schema(description = "Valor total del ingreso")
+        BigDecimal amount,
+        @Schema(description = "Mes en el que se va a registrar el ingreso")
+        Integer month,
+        @Schema(description = "Año en el que se va a registrar el ingreso")
+        Integer year
+) {
+}
